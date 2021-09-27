29d775da70be740976366c1358157e46905d2d84
Pranta07 <palpranta@gmail.com>
2021-09-27 23:11:38 +0600

    react-bootstrap card added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Row, Spinner } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import News from "./components/News/News";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [news, setNews] = useState([]);
+    useEffect(() => {
+        fetch(
+            "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=aafd38ac724446ac8538d32c92c1b109"
+        )
+            .then((res) => res.json())
+            .then((data) => setNews(data.articles));
+    }, []);
+
+    return (
+        <div className="App">
+            <Row xs={1} md={3} className="g-4">
+                {news.map((nw) => (
+                    <News news={nw}></News>
+                ))}
+            </Row>
+        </div>
+    );
 }
 
 export default App;
